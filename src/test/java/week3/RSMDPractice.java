76f939319fbd37f20c95aab4c7c788d4d7fcb469
pakdil <eaasbb@gmail.com>
2022-12-03 18:39:21 +0300

    RSMDPractice First commit

---
+++ src/test/java/week3/RSMDPractice.java
@@ -0,0 +1,30 @@
+package week3;
+
+import java.sql.*;
+
+public class RSMDPractice {
+
+    public static void main(String[] args) throws SQLException {
+
+        String dbUrl = "jdbc:oracle:thin:@54.234.104.66:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        Connection connection = DriverManager.getConnection(dbUrl, username, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * from Regions");
+
+       ResultSetMetaData rsmd = resultSet.getMetaData();
+
+        System.out.println(rsmd.getColumnName(2));
+        System.out.println(rsmd.getColumnCount());
+
+        for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+            System.out.println(rsmd.getColumnName(i));
+        }
+    }
+
+    // practice
+    // print full address (address + postal code + city + state)
+    
+}
